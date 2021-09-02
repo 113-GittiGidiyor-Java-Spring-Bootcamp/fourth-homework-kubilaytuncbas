1e5ebb13575b505e6706d2b62ea19d2c62d3a69f
Kubilay Tunçbaş <tuncbaskubilay@gmail.com>
2021-09-02 16:38:40 +0300

    packages

---
+++ demo/odev4GgSpringB/src/test/java/com/example/odev4ggspringb/Odev4GgSpringBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.odev4ggspringb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Odev4GgSpringBApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
